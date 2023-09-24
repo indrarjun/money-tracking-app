e59b5d28856d1fa3a309590d7f8cf984a65d4fa0
Indrarjun <billasaketh@gmail.com>
2023-09-24 15:27:26 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+// import { response } from "express";
 // require("dotenv").config();
 
 function App() {
   const [name, setName] = useState("");
   const [datetime, setDatetime] = useState("");
   const [description, setDescription] = useState("");
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    getTransactions().then((transactions) => {
+      setTransactions(transactions);
+    });
+  }, [transactions]);
+
+  async function getTransactions() {
+    const url = process.env.REACT_APP_API_URL + "/transactions";
+    const response = await fetch(url);
+    return await response.json();
+  }
 
   function addNewTransaction(ev) {
     ev.preventDefault();
-    const url = process.env.REACT_APP_API_URL;
-    console.log(url);
-    fetch(url);
+    const url = process.env.REACT_APP_API_URL + "/transaction";
+    // console.log(url);
+    const price = name.split(" ")[0];
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        price,
+        name: name.substring(price.length + 1),
+        description,
+        datetime,
+      }),
+    }).then((response) => {
+      response.json().then((json) => {
+        setName("");
+        setDatetime("");
+        setDescription("");
+        console.log("result", json);
+      });
+    });
+  }
+  let balance = 0;
+  for (const transcation of transactions) {
+    balance += transcation.price;
   }
 
+  balance = balance.toFixed(2);
+  const fraction = balance.split(".")[1];
+  balance = balance.split(".")[0];
   return (
     <main>
       <h1>
-        $400 <span>.00</span>
+        ${balance} <span>{fraction}</span>
       </h1>
       <form onSubmit={addNewTransaction}>
         <div className="basic">
@@ -47,41 +85,28 @@ function App() {
 
       {/* Transaction details */}
       <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">it was time for new tv</div>
-          </div>
+        {transactions.length > 0 &&
+          transactions.map((transcation) => {
+            return (
+              <div className="transaction">
+                <div className="left">
+                  <div className="name">{transcation.name}</div>
+                  <div className="description">{transcation.description}</div>
+                </div>
 
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Iphone</div>
-            <div className="description">it was time for new tv</div>
-          </div>
-
-          <div className="right">
-            <div className="price red">-$900</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
-
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Gig job new website</div>
-            <div className="description">it was time for new tv</div>
-          </div>
-
-          <div className="right">
-            <div className="price green">+$500</div>
-            <div className="datetime">2022-12-18 15:45</div>
-          </div>
-        </div>
+                <div className="right">
+                  <div
+                    className={
+                      "price " + (transcation.price < 0 ? "red" : "green")
+                    }
+                  >
+                    {transcation.price}
+                  </div>
+                  <div className="datetime">{transcation.datetime}</div>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </main>
   );
